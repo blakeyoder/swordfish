eb2c2de2f0fe095ed7eee962937a7ff1ce160b7c
blakeyoder <yoderblake@gmail.com>
2015-12-21 19:09:22 -0500

    Added close button on modal

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -14,22 +14,26 @@ $(document).ready(function(){
     $('.alert').fadeOut();
   }, 5000);
 
-  $(window).scroll(function () {
-    /* Check the location of each desired element */
-    $('.thumbnail').each(function () {
+    $(window).scroll(function () {
+      /* Check the location of each desired element */
+      $('.thumbnail').each(function () {
 
-        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
+          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-        /* If the object is completely visible in the window, fade it in */
-        if (bottom_of_window > bottom_of_object) {
+          /* If the object is completely visible in the window, fade it in */
+          if (bottom_of_window > bottom_of_object) {
 
-            $(this).animate({
-                'opacity': '1'
-            }, 650);
+              $(this).animate({
+                  'opacity': '1'
+              }, 650);
 
-         }
-     });
-});
+           }
+       });
+   });
+
+  $('#close').click(function(){
+    $('.modal').modal('hide')
+  });
 
 });
